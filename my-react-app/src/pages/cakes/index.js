48f410e4be706c9b5edf8f3c8795bed1219ca42f
unknown <171305433+MudhithaK@users.noreply.github.com>
2025-09-18 20:23:48 +0530

    add files

---
+++ my-react-app/src/pages/cakes/index.js
@@ -0,0 +1,8 @@
+import { IoIosArrowRoundForward } from "react-icons/io";
+import React, { useContext, useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const Cake=(pros)=>{
+
+}
+export default Cake;
