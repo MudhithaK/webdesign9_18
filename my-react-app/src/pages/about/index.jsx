927f8c1919754f87682eddede38efcc6cfa8d2c8
unknown <171305433+MudhithaK@users.noreply.github.com>
2025-09-26 11:34:06 +0530

    change

--- my-react-app/src/pages/about/index.jsx
+++ my-react-app/src/pages/about/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import Header from '../../Components/header';
+import "./abouts.css";
 
 const About = () => {
   return (
-    <div>
+    <>
         <Header/>
         <body>
 
         <div class="about-col" >
             <h1>How can you get help from us to plan your Wedding </h1>
-            <p>We provide the packages that need for a wedding with their cost. You can easily find the details of those  wedding packages and plan your wedding with the best selections from given packages.  Select packages as you want, and get budget for Wedding
+            <p>We provide the packages that need for a wedding with their cost.</p><p> You can easily find the details of those  wedding packages and plan your wedding with the best selections from given packages.  Select packages as you want, and get budget for Wedding
                 </p>
 
         </div>
         </body>
-    </div>
+    </>
   );
 }
 export default About;
